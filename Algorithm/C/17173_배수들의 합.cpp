83e22c49d8b68996b68e2707e110bd3899c19f93
neng2 <rkdwjdgh0617@gmail.com>
2024-07-15 15:22:34 +0900

    Remove .cpp files and Algorithm directory from tracking

--- Algorithm/C/17173_배수들의 합.cpp
+++
@@ -1,8 +0,0 @@
-#import<ios>
-int n,m,x,s,a[1010],i;main(){scanf("%d%d",&n,&m);while(m--){scanf("%d",&x);for(i=x;i<=n;i+=x)if(!a[i]){a[i]=1;s+=i;}}printf("%d",s);}
-/*
-수학
-구현
-브루트포스 알고리즘
-숏코딩
-*/
--- Algorithm/C/2438_별 찍기 1.cpp
+++
@@ -1,7 +0,0 @@
-#import<ios>
-char c[99],n;main(){for(scanf("%d",&n);n--;puts(c+n))c[n]=42;}
-
-/*
-숏코딩
-구현
-*/
